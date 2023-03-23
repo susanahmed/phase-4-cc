2ed6bc6a587aa7af73d592eed4168e3aae6fa3b9
Susan Ahmed <susanahmed617@gmail.com>
2023-03-23 11:45:53 -0400

    submit cc

--- server/app.py
+++ server/app.py
@@ -21,10 +21,87 @@ api = Api(app)
 def index():
     return '<h1>Code challenge</h1>'
 
-@app.route('/restaurants')
+@app.route('/restaurants', methods = ['GET'])
 def restaurants():
+    restaurants = Restaurant.query.all()
+    restaurants_dict = [restaurant.to_dict() for restaurant in restaurants]
 
-    pass
+    response = make_response(
+        jsonify(restaurants_dict),
+        200
+    )
+    return response
+
+@app.route('/restaurants/<int:id>', methods = ['GET', 'DELETE'])
+def restaurantsByID(id):
+    restaurant = Restaurant.query.filter_by(id = id).first()
+    
+    if restaurant:
+
+        if request.method == 'GET':
+            restaurant_dict = restaurant.to_dict()
+            
+            response = make_response(
+                jsonify(restaurant_dict),
+                200
+            )
+        else:
+            response = make_response(
+                {"error": "Restaurant not found"},
+                404
+            )
+    elif request.method == 'DELETE':
+        if restaurant:
+            db.session.delete(restaurant)
+            db.session.commit()
+
+            response = make_response(
+                {}, 
+                200
+            )
+        else:
+            response = make_response(
+                {"error": "Restaurant not found"},
+                404
+            )
+    return response
+
+@app.route('/pizzas', methods = ['GET'])
+def pizzas():
+    pizzas = Pizza.query.all()
+    pizzas_dict = [pizza.to_dict() for pizza in pizzas]
+
+    response = make_response(
+        jsonify(pizzas_dict),
+        200
+    )
+    return response
+
+@app.route('/restaurant_pizzas', methods = ['POST'])
+def restaurantPizza():
+    try: 
+        new_rest_pizza = RestaurantPizza(
+            price = request.get_json()['price'],
+            pizza_id = request.get_json()['pizza_id'],
+            restaurant_id = request.get_json()['restaurant_id']
+        )
+        
+        new_rest_pizza_dict = new_rest_pizza.to_dict()
+
+        db.session.add(new_rest_pizza)
+        db.session.commit()
+
+        response = make_response(
+            jsonify(new_rest_pizza_dict),
+            201
+        )
+    except ValueError:
+        response = make_response(
+            {"errors": ["validation errors"]},
+            400
+        )
+
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -10,4 +10,44 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
-# Add models here
+class Pizza(db.Model, SerializerMixin):
+    __tablename__ = 'pizzas'
+    serialize_rules = ('-restaurant_pizzas.pizza', '-restaurants.pizzas')
+
+    id = db.Column(db.Integer, primary_key=True)
+    name= db.Column(db.String)
+    ingredients = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref = 'pizza')
+
+class Restaurant(db.Model, SerializerMixin):
+    __tablename__= 'restaurants'
+    serialize_rules = ('-restaurant_pizzas.restaurant', '-pizzas.restaurants')
+
+    id = db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String)
+    address =db.Column(db.String)
+
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref = 'restaurant')
+
+class RestaurantPizza(db.Model, SerializerMixin):
+    __tablename__ = 'restaurant_pizzas'
+    serialize_rules = ('-restaurant.restaurant_pizzas', '-pizza.restaurant_pizzas')
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    @validates('price')
+    def validates_price(self, key, value):
+        if not value > 0 and not value < 30:
+            raise ValueError ("Price must be between 1 and 30")
+        return value
+
